52b15559757ede0b2ea202f9d6230352f2bd7eef
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-04-06 17:43:19 +0200

    fix: Correccion de test

--- src/app/modules/account/account.page.spec.ts
+++ src/app/modules/account/account.page.spec.ts
@@ -5,15 +5,18 @@ import {
     waitForAsync,
 } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { ComponentsModule } from 'src/app/components/components.module';
-import { AuthService } from 'src/app/services';
+import { OptionItemI } from 'src/app/interfaces/option-item.interface';
+import { AuthService, AlertService } from 'src/app/services';
 
 import { AccountPage } from './account.page';
 
 describe('AccountPage', () => {
     let component: AccountPage;
     let authService: AuthService;
+    let alertService: AlertService;
+    let navCtrl: NavController;
     let fixture: ComponentFixture<AccountPage>;
 
     beforeEach(waitForAsync(() => {
@@ -24,13 +27,15 @@ describe('AccountPage', () => {
                 RouterTestingModule,
                 ComponentsModule,
             ],
-            providers: [AuthService],
+            providers: [AuthService, AlertService],
         }).compileComponents();
 
         const testbed = getTestBed();
         fixture = TestBed.createComponent(AccountPage);
         component = fixture.componentInstance;
         authService = testbed.inject(AuthService);
+        alertService = testbed.inject(AlertService);
+        navCtrl = testbed.inject(NavController);
         fixture.detectChanges();
     }));
 
@@ -57,9 +62,39 @@ describe('AccountPage', () => {
     });
 
     it('logout', () => {
-        spyOn(authService, 'logout');
+        spyOn(alertService, 'presentAlertWithButtons');
         component.logout();
-        expect(authService.logout).toHaveBeenCalled();
+        expect(alertService.presentAlertWithButtons).toHaveBeenCalled();
+    });
+
+    it('onLogoutClick', () => {
+        spyOn(authService, 'logout');
+        component.onLogoutClick();
         expect(component.logged).toBe(false);
+        expect(authService.logout).toHaveBeenCalled();
+    });
+
+    describe('onClickOption', () => {
+        it('navigateForward', () => {
+            spyOn(navCtrl, 'navigateForward');
+            const item: OptionItemI = {
+                route: 'perro',
+                name: '',
+                icon: '',
+            };
+            component.onClickOption(item);
+            expect(navCtrl.navigateForward).toHaveBeenCalled();
+        });
+
+        it('logout', () => {
+            spyOn(component, 'logout');
+            const item: OptionItemI = {
+                value: 'logout',
+                name: '',
+                icon: '',
+            };
+            component.onClickOption(item);
+            expect(component.logout).toHaveBeenCalled();
+        });
     });
 });
