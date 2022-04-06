233a5499c4a65a53d012ee95a329544381365486
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-04-06 10:12:55 +0200

    feat: Se añade animacion para transicion de paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { customAnimation } from './core/animations/animations';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [
         BrowserModule,
-        IonicModule.forRoot(),
+        IonicModule.forRoot({ navAnimation: customAnimation }),
         AppRoutingModule,
         IonicStorageModule.forRoot(),
     ],
