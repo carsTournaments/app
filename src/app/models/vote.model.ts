f2a7a0d15078d62c4d3d9438659dd8a5299b78fd
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-04-10 16:13:12 +0200

    feat: Nuevas mejoras

--- src/app/models/vote.model.ts
+++ src/app/models/vote.model.ts
@@ -1,6 +1,8 @@
 export class Vote {
     _id?: string;
     pairing: string; // PairingI;
+    round: string;
+    tournament: string;
     car: string; // CarI;
     created?: string;
     updated?: string;
@@ -8,6 +10,8 @@ export class Vote {
     constructor(data?: Vote) {
         this._id = data?._id;
         this.pairing = data?.pairing || '';
+        this.round = data?.round || '';
+        this.tournament = data?.tournament || '';
         this.car = data?.car || '';
         this.created = data?.created;
         this.updated = data?.updated;
