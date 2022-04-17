4b8831885e1186d69afb58704d2c8715c6143165
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-04-17 11:54:24 +0200

    feat: Se añade iOS + Correcciones header para iOS

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 import { Header } from './model/header.model';
 
 @Component({
@@ -8,8 +7,31 @@ import { Header } from './model/header.model';
     templateUrl: 'header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
     @Input() options: Header;
     @Output() segmentChanged: EventEmitter<any> = new EventEmitter();
-    constructor() {}
+    height: number;
+    heightPx: string;
+    constructor(private platform: Platform) {
+        this.getHeightToolbar();
+    }
+
+    ngOnInit(): void {
+        this.getHeightToolbar();
+    }
+
+    getHeightToolbar() {
+        if (
+            !this.options?.segments ||
+            this.options?.segments?.items.length === 0
+        ) {
+            this.height = 60;
+        } else {
+            this.height = 90;
+        }
+        if (this.platform.is('ios')) {
+            this.height += 50;
+        }
+        this.heightPx = this.height + 'px';
+    }
 }
