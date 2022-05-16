98027dbd3886e3135a169d9fa472ad09da739b6e
JoseXS <XSkunk@gmail.com>
2022-05-16 18:10:06 +0200

    fix: Correcciones menores

--- src/app/modules/car/components/car-totals/car-totals.component.ts
+++ src/app/modules/car/components/car-totals/car-totals.component.ts
@@ -6,8 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['./car-totals.component.scss'],
 })
 export class CarTotalsComponent implements OnInit {
-    @Input() likes: { count: number } = { count: 0 };
     @Input() votes: { count: number } = { count: 0 };
+    @Input() likes: { count: number } = { count: 0 };
     @Input() gold: { count: number } = { count: 0 };
     @Input() silver: { count: number } = { count: 0 };
     @Input() bronze: { count: number } = { count: 0 };
