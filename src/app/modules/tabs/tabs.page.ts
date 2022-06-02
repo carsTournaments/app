b780260f95b1430b57d8a9a35271d8c1267d7239
JoseXS <XSkunk@gmail.com>
2022-06-02 13:02:50 +0200

    feat: Se añaden toggles a tabs

--- src/app/modules/tabs/tabs.page.ts
+++ src/app/modules/tabs/tabs.page.ts
@@ -1,35 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TogglesService } from '@core/toggles/toggles.service';
 import { AnalyticsService } from '@services';
+import { TabI } from './../../shared/interfaces/tab.interface';
 
 @Component({
     selector: 'app-tabs',
     templateUrl: 'tabs.page.html',
     styleUrls: ['./tabs.page.scss'],
 })
-export class TabsPage {
-    tabs = [
-        {
-            name: 'Torneos',
-            route: 'tournaments',
-            icon: 'list-outline',
-        },
-        {
-            name: 'Calendario',
-            route: 'calendar',
-            icon: 'calendar-outline',
-        },
-        {
-            name: 'Coches',
-            route: 'cars',
-            icon: 'car-outline',
-        },
-        {
-            name: 'Tu cuenta',
-            route: 'account',
-            icon: 'person-circle-outline',
-        },
-    ];
-    constructor(private analyticsService: AnalyticsService) {}
+export class TabsPage implements OnInit {
+    tabs = [];
+    constructor(
+        private analyticsService: AnalyticsService,
+        private togglesService: TogglesService
+    ) {}
+
+    ngOnInit() {
+        this.generateTabs();
+    }
+
+    async generateTabs() {
+        this.tabs = [];
+        const items: TabI[] = [
+            {
+                name: 'Torneos',
+                route: 'tournaments',
+                icon: 'list-outline',
+                toggle: 'tab_tournaments',
+            },
+            {
+                name: 'Calendario',
+                route: 'calendar',
+                icon: 'calendar-outline',
+                toggle: 'tab_calendar',
+            },
+            {
+                name: 'Coches',
+                route: 'cars',
+                icon: 'car-outline',
+                toggle: 'tab_cars',
+            },
+            {
+                name: 'Tu cuenta',
+                route: 'account',
+                icon: 'person-circle-outline',
+                toggle: 'tab_account',
+            },
+        ];
+
+        for (const item of items) {
+            if (await this.togglesService.isActiveToggle(item.toggle)) {
+                this.tabs.push(item);
+            }
+        }
+    }
 
     clickTab(name: string) {
         this.analyticsService.logEvent(`tabs_${name}`);
---
+++ src/app/shared/interfaces/tab.interface.ts
@@ -0,0 +1,6 @@
+export class TabI {
+    name: string;
+    route: string;
+    icon: string;
+    toggle?: string;
+}
