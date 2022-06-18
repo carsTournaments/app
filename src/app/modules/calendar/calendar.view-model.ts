4209da73ad74c3a9a9b59919d4997ad457db702a
JoseXS <XSkunk@gmail.com>
2022-06-19 01:47:06 +0200

    feat: Se añaden cambios en calendario para recibir torneos todo

--- src/app/modules/calendar/calendar.view-model.ts
+++ src/app/modules/calendar/calendar.view-model.ts
@@ -1,9 +1,10 @@
 import { Header } from '@components/header/model/header.model';
-import { Round } from '@models';
+import { Round, Tournament } from '@models';
 
 export class CalendarViewModel {
     header = new Header({ title: 'Calendario' });
     dateSelected = '';
     rounds: Round[] = [];
+    tournaments: Tournament[] = [];
     loading = true;
 }
