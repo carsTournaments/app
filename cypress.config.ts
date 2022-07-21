2a6ef684e2c7ab4afccce52b90cd4d0b73663c5c
JoseXS <XSkunk@gmail.com>
2022-07-22 01:07:24 +0200

    fix: Correccion cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,19 +4,6 @@ export default defineConfig({
   e2e: {
     projectId: "9jwmn3",
     baseUrl: 'http://localhost:8100',
-    arch: 'arm64',
-    browsers: [
-      {
-        name: 'chrome',
-        family: 'chromium',
-        channel: 'stable',
-        displayName: 'Chrome',
-        version: '103.0.5060.134',
-        path: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-        minSupportedVersion: 64,
-        majorVersion: 103,
-      },
-    ],
     env: {
     //   url: 'https://carstournaments.com'
     url: 'http://localhost:8100'
