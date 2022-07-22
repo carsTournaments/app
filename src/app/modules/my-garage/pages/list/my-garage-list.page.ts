f622da841765120f414352f1c659e31b3289f11d
JoseXS <XSkunk@gmail.com>
2022-07-22 16:43:03 +0200

    fix: Correcciones prettier

--- src/app/modules/my-garage/pages/list/my-garage-list.page.ts
+++ src/app/modules/my-garage/pages/list/my-garage-list.page.ts
@@ -1,143 +1,143 @@
 import { Component } from '@angular/core';
 import { config } from '@config';
 import {
-  NavController,
-  PopoverController,
-  PopoverOptions,
+    NavController,
+    PopoverController,
+    PopoverOptions,
 } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core';
 import { Car } from '@models';
 import { TranslatePipe } from '@ngx-translate/core';
 import {
-  AlertService,
-  CarService,
-  UtilsService,
-  UserService,
-  ImageService,
-  ToastIonicService,
+    AlertService,
+    CarService,
+    UtilsService,
+    UserService,
+    ImageService,
+    ToastIonicService,
 } from '@services';
 import { GaragePopoverComponent } from '../../components/popover/garage-popover.component';
 import { MyGarageListViewModel } from '../../models/my-garage-list.view-model';
 
 @Component({
-  selector: 'page-my-garage-list',
-  templateUrl: 'my-garage-list.page.html',
-  styleUrls: ['./my-garage-list.page.scss'],
-  providers: [TranslatePipe],
+    selector: 'page-my-garage-list',
+    templateUrl: 'my-garage-list.page.html',
+    styleUrls: ['./my-garage-list.page.scss'],
+    providers: [TranslatePipe],
 })
 export class MyGarageListPage {
-  vm = new MyGarageListViewModel();
-  constructor(
-    private carService: CarService,
-    private navCtrl: NavController,
-    private alertService: AlertService,
-    private popoverCtrl: PopoverController,
-    private imageService: ImageService,
-    private userService: UserService,
-    private utilsService: UtilsService,
-    private translatePipe: TranslatePipe,
-    private toastIonicService: ToastIonicService
-  ) { }
+    vm = new MyGarageListViewModel();
+    constructor(
+        private carService: CarService,
+        private navCtrl: NavController,
+        private alertService: AlertService,
+        private popoverCtrl: PopoverController,
+        private imageService: ImageService,
+        private userService: UserService,
+        private utilsService: UtilsService,
+        private translatePipe: TranslatePipe,
+        private toastIonicService: ToastIonicService
+    ) {}
 
-  async ionViewWillEnter(): Promise<void> {
-    this.getAllCars();
-  }
-
-  async getAllCars(): Promise<void> {
-    this.vm.user = this.userService.getUser();
-    this.vm.bodyCars.id = this.vm.user._id;
-    this.carService.getAllOfDriver(this.vm.bodyCars).subscribe({
-      next: (response) => {
-        this.vm.cars = response;
-        this.vm.loading = false;
-        this.vm.error = false;
-      },
-      error: () => {
-        this.vm.loading = false;
-        this.vm.error = true;
-      },
-    });
-  }
-
-  async openPopover(e: any, car: Car): Promise<void> {
-    const options: PopoverOptions = {
-      component: GaragePopoverComponent,
-      event: e,
-      mode: 'ios',
-      cssClass: 'popover-garage',
-      reference: 'event',
-    };
-    const popover = await this.popoverCtrl.create(options);
-    popover.present();
-    popover
-      .onDidDismiss()
-      .then((data) => this.onDidDismissPopover(data, car));
-  }
-
-  onDidDismissPopover(data: OverlayEventDetail<any>, car: Car): void {
-    if (data.data) {
-      if (data.data === 'viewProfile') {
-        this.navCtrl.navigateForward(
-          config.routes.car.replace(':id', car._id)
-        );
-      } else if (data.data === 'edit') {
-        this.editCar(car);
-      } else if (data.data === 'image') {
-        this.addImage(car);
-      } else {
-        this.deleteCar(car);
-      }
-    }
-  }
-
-  editCar(car: Car): void {
-    this.navCtrl.navigateForward(
-      config.routes.myGarageOne.replace(':id', car._id)
-    );
-  }
-
-  async addImage(car: Car): Promise<void> {
-    this.imageService.addNewToGallery('car', car._id).then(
-      () => this.utilsService.reloadPage(),
-      (error) => this.alertService.presentAlert('Error', error)
-    );
-  }
-
-  async deleteCar(car: Car): Promise<void> {
-    await this.alertService.presentAlertWithButtons(
-      this.translatePipe.transform('garageList.titleDeleteCar'),
-      this.translatePipe.transform('garageList.messageDeleteCar'),
-      [
-        {
-          text: this.translatePipe.transform('generic.no'),
-          role: 'cancel',
-        },
-        {
-          text: this.translatePipe.transform('generic.yes'),
-          role: 'ok',
-          handler: () => this.deleteCarConfirmation(car),
-        },
-      ]
-    );
-  }
-
-  async deleteCarConfirmation(car: Car): Promise<void> {
-    this.carService.delete(car._id).subscribe({
-      next: () => {
+    async ionViewWillEnter(): Promise<void> {
         this.getAllCars();
-        this.toastIonicService.info(
-          this.translatePipe.transform(
-            'garageList.messageDeleteCarConfirmation'
-          )
-        );
-      },
-      error: (error) => {
-        this.alertService.presentAlert('Error', error);
-      },
-    });
-  }
+    }
 
-  onClickAddCar(): void {
-    this.navCtrl.navigateForward(config.routes.myGarageCreate);
-  }
+    async getAllCars(): Promise<void> {
+        this.vm.user = this.userService.getUser();
+        this.vm.bodyCars.id = this.vm.user._id;
+        this.carService.getAllOfDriver(this.vm.bodyCars).subscribe({
+            next: (response) => {
+                this.vm.cars = response;
+                this.vm.loading = false;
+                this.vm.error = false;
+            },
+            error: () => {
+                this.vm.loading = false;
+                this.vm.error = true;
+            },
+        });
+    }
+
+    async openPopover(e: any, car: Car): Promise<void> {
+        const options: PopoverOptions = {
+            component: GaragePopoverComponent,
+            event: e,
+            mode: 'ios',
+            cssClass: 'popover-garage',
+            reference: 'event',
+        };
+        const popover = await this.popoverCtrl.create(options);
+        popover.present();
+        popover
+            .onDidDismiss()
+            .then((data) => this.onDidDismissPopover(data, car));
+    }
+
+    onDidDismissPopover(data: OverlayEventDetail<any>, car: Car): void {
+        if (data.data) {
+            if (data.data === 'viewProfile') {
+                this.navCtrl.navigateForward(
+                    config.routes.car.replace(':id', car._id)
+                );
+            } else if (data.data === 'edit') {
+                this.editCar(car);
+            } else if (data.data === 'image') {
+                this.addImage(car);
+            } else {
+                this.deleteCar(car);
+            }
+        }
+    }
+
+    editCar(car: Car): void {
+        this.navCtrl.navigateForward(
+            config.routes.myGarageOne.replace(':id', car._id)
+        );
+    }
+
+    async addImage(car: Car): Promise<void> {
+        this.imageService.addNewToGallery('car', car._id).then(
+            () => this.utilsService.reloadPage(),
+            (error) => this.alertService.presentAlert('Error', error)
+        );
+    }
+
+    async deleteCar(car: Car): Promise<void> {
+        await this.alertService.presentAlertWithButtons(
+            this.translatePipe.transform('garageList.titleDeleteCar'),
+            this.translatePipe.transform('garageList.messageDeleteCar'),
+            [
+                {
+                    text: this.translatePipe.transform('generic.no'),
+                    role: 'cancel',
+                },
+                {
+                    text: this.translatePipe.transform('generic.yes'),
+                    role: 'ok',
+                    handler: () => this.deleteCarConfirmation(car),
+                },
+            ]
+        );
+    }
+
+    async deleteCarConfirmation(car: Car): Promise<void> {
+        this.carService.delete(car._id).subscribe({
+            next: () => {
+                this.getAllCars();
+                this.toastIonicService.info(
+                    this.translatePipe.transform(
+                        'garageList.messageDeleteCarConfirmation'
+                    )
+                );
+            },
+            error: (error) => {
+                this.alertService.presentAlert('Error', error);
+            },
+        });
+    }
+
+    onClickAddCar(): void {
+        this.navCtrl.navigateForward(config.routes.myGarageCreate);
+    }
 }
--- src/app/shared/components/car-add/car-add.component.ts
+++ src/app/shared/components/car-add/car-add.component.ts
@@ -2,225 +2,226 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Brand, Car } from '@models';
 import {
-  AlertService,
-  BrandService,
-  CarService,
-  ImageService,
-  ToastIonicService,
-  UserService,
-  UtilsService,
+    AlertService,
+    BrandService,
+    CarService,
+    ImageService,
+    ToastIonicService,
+    UserService,
+    UtilsService,
 } from '@services';
 import { BrandGetAllDto } from '@services/api/brand/brand.dto';
 
 @Component({
-  selector: 'car-add',
-  templateUrl: 'car-add.component.html',
+    selector: 'car-add',
+    templateUrl: 'car-add.component.html',
 })
 export class CarAddComponent implements OnInit {
-  @Input() car: Car = new Car();
-  @Input() brandIdSelected = '';
-  @Output() carAddSuccess: EventEmitter<void> = new EventEmitter();
-  brands: Brand[] = [];
-  brandsBody: BrandGetAllDto = {
-    page: 1,
-    pageSize: 100,
-    site: 'app',
-    order: ['name', 'asc'],
-  };
-  edit = false;
-  loading = true;
-
-  constructor(
-    private brandService: BrandService,
-    private userService: UserService,
-    private carService: CarService,
-    private alertService: AlertService,
-    private navCtrl: NavController,
-    private imageService: ImageService,
-    private utilsService: UtilsService,
-    private toastService: ToastIonicService
-  ) { }
-
-  ngOnInit(): void {
-    this.getAllBrands();
-  }
-
-  getAllBrands(): void {
-    this.brandService.getAll(this.brandsBody).subscribe({
-      next: (data) => this.onGetAllBrandsSuccess(data.items),
-      error: (err) => console.error(err),
-    });
-  }
-
-  onGetAllBrandsSuccess(items: Brand[]): void {
-    this.brands = items;
-    if (this.brandIdSelected !== '') {
-      this.edit = true;
-    }
-    this.loading = false;
-  }
-
-  async createOrUpdateItem(): Promise<void> {
-    if (this.createOrUpdateSetsAndValidations()) {
-      if (this.edit) {
-        this.car.driver = this.car.driver._id;
-        this.createOrUpdate();
-      } else {
-        this.car.driver = this.userService.getUser()._id;
-        if (this.car.driver) {
-          this.createOrUpdate();
-        } else {
-          this.alertService.presentAlert(
-            'Vaya',
-            'No se pudo obtener el usuario'
-          );
-        }
-      }
-    }
-  }
-
-  createOrUpdateSetsAndValidations(): boolean {
-    this.setData();
-    const validations = this.validations();
-    if (validations.state) {
-      return true;
-    } else {
-      this.alertService.presentAlert('¡Vaya!', validations.message);
-      return false;
-    }
-  }
-
-  setData(): void {
-    this.car.brand = this.brandIdSelected;
-    this.car.cc = this.car.cc ? Number(this.car.cc) : null;
-    this.car.cv = this.car.cv ? Number(this.car.cv) : null;
-    this.car.year = this.car.year ? Number(this.car.year) : null;
-  }
-
-  validations(): { state: boolean; message: string } {
-    const data = {
-      state: true,
-      message: '',
+    @Input() car: Car = new Car();
+    @Input() brandIdSelected = '';
+    @Output() carAddSuccess: EventEmitter<void> = new EventEmitter();
+    brands: Brand[] = [];
+    brandsBody: BrandGetAllDto = {
+        page: 1,
+        pageSize: 100,
+        site: 'app',
+        order: ['name', 'asc'],
     };
+    edit = false;
+    loading = true;
 
-    if (!this.car.brand) {
-      data.state = false;
-      data.message = 'La marca es obligatoria';
-      return data;
+    constructor(
+        private brandService: BrandService,
+        private userService: UserService,
+        private carService: CarService,
+        private alertService: AlertService,
+        private navCtrl: NavController,
+        private imageService: ImageService,
+        private utilsService: UtilsService,
+        private toastService: ToastIonicService
+    ) {}
+
+    ngOnInit(): void {
+        this.getAllBrands();
     }
 
-    if (!this.car.model) {
-      data.state = false;
-      data.message = 'El modelo es obligatorio';
-      return data;
+    getAllBrands(): void {
+        this.brandService.getAll(this.brandsBody).subscribe({
+            next: (data) => this.onGetAllBrandsSuccess(data.items),
+            error: (err) => console.error(err),
+        });
     }
 
-    if (!this.car.traction) {
-      data.state = false;
-      data.message = 'La traccion es obligatoria';
-      return data;
-    }
-
-    if (!this.car.fuel) {
-      data.state = false;
-      data.message = 'El combustible es obligatorio';
-      return data;
-    }
-
-    if (this.car.year) {
-      if (
-        this.car.year < 1900 ||
-        this.car.year > new Date().getFullYear()
-      ) {
-        data.state = false;
-        data.message = 'El año no es válido';
-        return data;
-      }
-    } else {
-      data.state = false;
-      data.message = 'El año es obligatorio';
-      return data;
-    }
-
-    if (this.car.cv) {
-      if (this.car.cv < 0 || this.car.cv > 1000) {
-        data.state = false;
-        data.message = 'Los CV no son válidos';
-        return data;
-      }
-    } else {
-      data.state = false;
-      data.message = 'Los CV son obligatorios';
-      return data;
-    }
-
-    if (this.car.cc) {
-      if (this.car.cc < 0 || this.car.cc > 5000) {
-        data.state = false;
-        data.message = 'Los CC no son válidos';
-        return data;
-      }
-    } else {
-      data.state = false;
-      data.message = 'Los CC son obligatorios';
-      return data;
-    }
-    return data;
-  }
-
-  createOrUpdate(): void {
-    const observable = this.edit
-      ? this.carService.update(this.car)
-      : this.carService.create(this.car);
-    observable.subscribe({
-      next: (newCar) => {
-        this.car = newCar;
-        this.edit = true;
-        if (!this.car.image) {
-          this.carNoImage();
-        } else {
-          this.toastService.info(
-            'El coche se ha actualizado correctamente'
-          );
-          this.carAddSuccess.emit();
+    onGetAllBrandsSuccess(items: Brand[]): void {
+        this.brands = items;
+        if (this.brandIdSelected !== '') {
+            this.edit = true;
         }
-      },
-      error: () =>
-        this.toastService.error(
-          'Ha ocurrido un error, intentalo mas tarde'
-        ),
-    });
-  }
-
-  async carNoImage() {
-    const alert = await this.alertService.presentAlertWithButtons(
-      '¡Listo!',
-      `El coche se ha ${this.edit ? 'actualizado' : 'creado'
-      }, pero aun no tiene ninguna foto, ¿quieres añadir una?`,
-      [
-        {
-          text: 'No',
-          role: 'cancel',
-        },
-        {
-          text: 'Sí',
-          role: 'ok',
-        },
-      ]
-    );
-
-    const data = await alert.onDidDismiss();
-    if (data.role === 'ok') {
-      this.addImage();
-    } else {
-      this.carAddSuccess.emit();
+        this.loading = false;
     }
-  }
 
-  async addImage(): Promise<void> {
-    this.imageService.addNewToGallery('car', this.car._id).then(
-      () => this.carAddSuccess.emit(),
-      (error) => this.alertService.presentAlert('Error', error)
-    );
-  }
+    async createOrUpdateItem(): Promise<void> {
+        if (this.createOrUpdateSetsAndValidations()) {
+            if (this.edit) {
+                this.car.driver = this.car.driver._id;
+                this.createOrUpdate();
+            } else {
+                this.car.driver = this.userService.getUser()._id;
+                if (this.car.driver) {
+                    this.createOrUpdate();
+                } else {
+                    this.alertService.presentAlert(
+                        'Vaya',
+                        'No se pudo obtener el usuario'
+                    );
+                }
+            }
+        }
+    }
+
+    createOrUpdateSetsAndValidations(): boolean {
+        this.setData();
+        const validations = this.validations();
+        if (validations.state) {
+            return true;
+        } else {
+            this.alertService.presentAlert('¡Vaya!', validations.message);
+            return false;
+        }
+    }
+
+    setData(): void {
+        this.car.brand = this.brandIdSelected;
+        this.car.cc = this.car.cc ? Number(this.car.cc) : null;
+        this.car.cv = this.car.cv ? Number(this.car.cv) : null;
+        this.car.year = this.car.year ? Number(this.car.year) : null;
+    }
+
+    validations(): { state: boolean; message: string } {
+        const data = {
+            state: true,
+            message: '',
+        };
+
+        if (!this.car.brand) {
+            data.state = false;
+            data.message = 'La marca es obligatoria';
+            return data;
+        }
+
+        if (!this.car.model) {
+            data.state = false;
+            data.message = 'El modelo es obligatorio';
+            return data;
+        }
+
+        if (!this.car.traction) {
+            data.state = false;
+            data.message = 'La traccion es obligatoria';
+            return data;
+        }
+
+        if (!this.car.fuel) {
+            data.state = false;
+            data.message = 'El combustible es obligatorio';
+            return data;
+        }
+
+        if (this.car.year) {
+            if (
+                this.car.year < 1900 ||
+                this.car.year > new Date().getFullYear()
+            ) {
+                data.state = false;
+                data.message = 'El año no es válido';
+                return data;
+            }
+        } else {
+            data.state = false;
+            data.message = 'El año es obligatorio';
+            return data;
+        }
+
+        if (this.car.cv) {
+            if (this.car.cv < 0 || this.car.cv > 1000) {
+                data.state = false;
+                data.message = 'Los CV no son válidos';
+                return data;
+            }
+        } else {
+            data.state = false;
+            data.message = 'Los CV son obligatorios';
+            return data;
+        }
+
+        if (this.car.cc) {
+            if (this.car.cc < 0 || this.car.cc > 5000) {
+                data.state = false;
+                data.message = 'Los CC no son válidos';
+                return data;
+            }
+        } else {
+            data.state = false;
+            data.message = 'Los CC son obligatorios';
+            return data;
+        }
+        return data;
+    }
+
+    createOrUpdate(): void {
+        const observable = this.edit
+            ? this.carService.update(this.car)
+            : this.carService.create(this.car);
+        observable.subscribe({
+            next: (newCar) => {
+                this.car = newCar;
+                this.edit = true;
+                if (!this.car.image) {
+                    this.carNoImage();
+                } else {
+                    this.toastService.info(
+                        'El coche se ha actualizado correctamente'
+                    );
+                    this.carAddSuccess.emit();
+                }
+            },
+            error: () =>
+                this.toastService.error(
+                    'Ha ocurrido un error, intentalo mas tarde'
+                ),
+        });
+    }
+
+    async carNoImage() {
+        const alert = await this.alertService.presentAlertWithButtons(
+            '¡Listo!',
+            `El coche se ha ${
+                this.edit ? 'actualizado' : 'creado'
+            }, pero aun no tiene ninguna foto, ¿quieres añadir una?`,
+            [
+                {
+                    text: 'No',
+                    role: 'cancel',
+                },
+                {
+                    text: 'Sí',
+                    role: 'ok',
+                },
+            ]
+        );
+
+        const data = await alert.onDidDismiss();
+        if (data.role === 'ok') {
+            this.addImage();
+        } else {
+            this.carAddSuccess.emit();
+        }
+    }
+
+    async addImage(): Promise<void> {
+        this.imageService.addNewToGallery('car', this.car._id).then(
+            () => this.carAddSuccess.emit(),
+            (error) => this.alertService.presentAlert('Error', error)
+        );
+    }
 }
