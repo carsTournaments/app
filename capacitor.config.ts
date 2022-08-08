a877b25d1068f408451963845cebfb795ee3a0f3
JoseXS <XSkunk@gmail.com>
2022-08-08 18:40:23 +0200

    feat: Mejoras para capacitor 4 + app-update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,28 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.josexs.ct',
-    appName: 'CarsTournaments',
-    webDir: 'www',
-    bundledWebRuntime: false,
-    plugins: {
-        CapacitorUpdater: {
-            autoUpdate: true,
-            autoUpdateUrl:
-                'https://api.carstournaments.com/ota/getOtaAvailable/',
-        },
-        PushNotifications: {
-            presentationOptions: ['badge', 'sound', 'alert'],
-        },
-        GoogleAuth: {
-            scopes: ['profile', 'email'],
-            serverClientId:
-                '432785685251-2q1rfgi63ssrf8dhim75ao5dsudh3a6g.apps.googleusercontent.com',
-        },
+  appId: 'com.josexs.ct',
+  appName: 'CarsTournaments',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    CapacitorUpdater: {
+      autoUpdate: true,
+      autoUpdateUrl: 'https://api.carstournaments.com/ota/getOtaAvailable/',
     },
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com'],
+    },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId:
+        '432785685251-2q1rfgi63ssrf8dhim75ao5dsudh3a6g.apps.googleusercontent.com',
+    },
+  },
 };
 
 export default config;
