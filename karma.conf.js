15cb52add0bf27089b9710fc677317fad826304a
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-09-11 23:56:10 +0200

    fix: Correcciones

--- karma.conf.js
+++ karma.conf.js
@@ -2,45 +2,45 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage'),
-            require('@angular-devkit/build-angular/plugins/karma'),
-        ],
-        client: {
-            jasmine: {
-                // you can add configuration options for Jasmine here
-                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-                // for example, you can disable the random execution with `random: false`
-                // or set a specific seed with `seed: 4321`
-            },
-            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/ngv'),
+      subdir: '.',
+      reporters: [{ type: 'lcov' }, { type: 'text-summary' }],
+      check: {
+        global: {
+          statements: 50,
         },
-        jasmineHtmlReporter: {
-            suppressAll: true, // removes the duplicated traces
-        },
-        coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/ngv'),
-            subdir: '.',
-          reporters: [{ type: 'html' }, { type: 'lcov', subdir: 'report-lcov' }, { type: 'text-summary' }],
-            check: {
-                global: {
-                    statements: 50,
-                },
-            },
-        },
-        reporters: ['progress', 'kjhtml', 'coverage'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['Chrome'],
-        singleRun: false,
-        restartOnFileChange: true,
-    });
+      },
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true,
+  });
 };
