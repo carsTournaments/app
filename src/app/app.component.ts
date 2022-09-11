d8a7f4ce0064a0e4e3842a07c730c50091a64dd4
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-09-11 22:07:54 +0200

    fix: Correcciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,6 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(
